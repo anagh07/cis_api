89d4a7d6f60ecb70c6d7dd5b2ec09807b930ec05
Anagh Mehran <anagh07@gmail.com>
2021-10-27 07:27:22 -0400

    Default route

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,24 @@
-// setup server entry point here
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+// Body parser
+app.use(express.json());
+
+// CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+});
+
+// Default route
+app.get('/', (req, res, next) => {
+    res.send('Server running')
+})
+
+// Initialize server
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
