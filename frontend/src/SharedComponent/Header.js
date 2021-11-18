3d379c1e5dfb9c73fcbb3603a16e984221fe9f6d
Zeina Kittaneh <zeinakittaneh@gmail.com>
2021-11-18 00:13:19 -0500

    adding logout button and hide login/signup buttons

--- frontend/src/SharedComponent/Header.js
+++ frontend/src/SharedComponent/Header.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, css } from 'aphrodite';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { MenuItem } from '@material-ui/core';
 import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
 
 export const styles = StyleSheet.create({
   appBar: {
@@ -18,24 +20,51 @@ export const styles = StyleSheet.create({
   }
 });
 
-function Header() {
-  return (
-    <AppBar position='fixed' className={css(styles.appBar)}>
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  loading: state.auth.loading,
+  token: state.auth.token,
+  user: state.auth.user,
+});
+
+console.log(localStorage.token, mapStateToProps.isAuthenticated);
+
+class Header extends Component {
+  constructor(props) {
+    super(props);    
+  }
+
+  
+handleLogout = (event) => {
+  localStorage.clear();
+  window.location.href = '/';
+}
+
+  render() {
+    return (
+      <AppBar position='fixed' className={css(styles.appBar)}>
       <Toolbar>
         <MenuItem>
           <Link to="/" className={css(styles.menuLink)}>
             COVID Info System
           </Link>
         </MenuItem>
+        { ( (localStorage.token === '') || (!localStorage.token)|| (this.props.isAuthenticated === false)  )?
+        <div style={{ marginLeft: "auto", marginRight: -12 }}>
+            <Link to="/signup" className={css(styles.menuLink)} style = {{marginRight: "20px"}}>Signup</Link>
+            <Link to="/login" className={css(styles.menuLink)} style = {{marginRight: "20px"}}>Login</Link>
+        </div>
+        :
         <MenuItem style={{ marginLeft: "auto", marginRight: -12 }}>
-          <Link to="/signup" className={css(styles.menuLink)}>Signup</Link>
+          <Link to="/" className={css(styles.menuLink)} onClick={this.handleLogout}>Logout</Link>
         </MenuItem>
-        <MenuItem className={css(styles.menuItem)}>
-          <Link to="/login" className={css(styles.menuLink)}>Login</Link>
-        </MenuItem>
+        }
       </Toolbar>
     </AppBar>
-  );
+
+    );
+  }
 }
 
-export default Header;
+export default connect(mapStateToProps, {})(Header);
